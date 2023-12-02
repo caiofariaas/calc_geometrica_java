84358e53b9dade64b6f901663795c95f97a98ccd
caiofariaas <caiofarias077@gmail.com>
2023-12-02 17:25:35 -0300

    newV

--- src/Main.java
+++ src/Main.java
@@ -5,9 +5,16 @@ import javax.swing.*;
 
 public class Main {
     public static void main(String[] args) {
-        String nome = JOptionPane.showInputDialog("Bem-Vindo a Calculadora!!\nQual o seu nome?");
+        String nome = JOptionPane.showInputDialog("Bem-Vindo(a) a Calculadora Geometrica!!\nQual o seu nome?");
 
-        
+        int opt = Integer.parseInt(JOptionPane.showInputDialog("Você deseja calcular figuras planas ou espaciais?"));
+
+        switch (opt){
+            case 1 ->{
+
+            }
+        }
+
 
     }
 }
