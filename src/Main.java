c496cfbbbef637b5e0093132efc86c73951ef050
caiofariaas <caiofarias077@gmail.com>
2023-12-02 17:23:11 -0300

    java

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,13 @@
 import Espaciais.*;
 import Planas.*;
 
+import javax.swing.*;
+
 public class Main {
     public static void main(String[] args) {
-        HexagonoRegular hexagonoRegular = new HexagonoRegular(5);
-        Retangulo retangulo = new Retangulo(10, 20);
+        String nome = JOptionPane.showInputDialog("Bem-Vindo a Calculadora!!\nQual o seu nome?");
 
-        System.out.printf("%.2f\n",hexagonoRegular.calcularArea());
-        System.out.printf("%.2f \n", hexagonoRegular.calcularPerimetro());
+        
 
-        System.out.println(retangulo.toString(2));
     }
 }
